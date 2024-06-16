c2fb7b85e1cc35f02ec1c5e78b4a7c2cf4304ef6
Sanjeev Midda <110365686+SanjeevMidda@users.noreply.github.com>
2024-06-16 10:44:10 +0100

    Add files via upload
    Uploading source files.

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import "./index.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="imageContainer">
+        <svg
+          width="288"
+          height="154"
+          viewBox="0 0 288 154"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          id="wave"
+        >
+          <path
+            d="M1 77.0065C1 77.0065 83 -94.9935 144 77.0065C205 249.006 287 77.0065 287 77.0065"
+            stroke="white"
+          />
+        </svg>
+      </div>
+
+      <h1>wave.</h1>
+    </div>
+  );
+}
+
+export default App;
